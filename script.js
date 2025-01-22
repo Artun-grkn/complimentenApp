dab429b2ea4cc95ab7a5b4cb9d2e56689800889e
Gürkan Artun <artun.gurkan@leerling.gent.be>
2025-01-22 10:51:53 +0100

    we hebben de complimenten random gemaakt

--- script.js
+++ script.js
@@ -1,5 +1,44 @@
+//fetch compliments
+async function fetchCompliments() {
+   try {
+        const response = await fetch('./data/complimenten.json')
+        console.log(response);
+        if (!response.ok) {
+            throw new Error("ik kan de json file niet laden. Check spelling en pad")
+        }
+        const data = await response.json(); 
+        console.log(data);
+        return data.complimenten;
+   } catch (error) {
+    console.error("ik kan de complimenten niet verkrijgen",error);
+    return["je bent geweldig! , Blijf stralen , Code met passie"];
+
+   } finally { //finally = close file / break
+
+   };
+}
+   
+
+
+
+//display compliments
+function displayRandomCompliment(compliments) {
+    const complimentElement = document.getElementById('compliment');
+    
+    const randomCompliment = compliments [Math.floor(Math.random() * compliments.length)];
+    console.log(randomCompliment);
+    complimentElement.textContent = randomCompliment;
+
+    
+};
+
+
+
 // main function IIFE (immediately invoked function expression)
-(async()=>{
+(async()=>{  //async = HOLD/WAIT
     const compliments = await fetchCompliments();
-    await
-})()
+    const button = document.getElementById('generate-btn');
+    //luister naar de button
+    button.addEventListener('click', () => displayRandomCompliment(compliments) ); //luister naar de button als er geklikt wordt wordt er een compliment gegeven
+})();
+
